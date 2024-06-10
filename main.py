651c9e8c3a8ccc1c49da0d41797244eba839d0d6
railira <rlil@uc.cl>
2024-06-10 18:53:20 -0400

    main completo

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import subprocess
 path1 = 'src/scraper/antartica.py'
 path2 = 'src/scraper/feriachilena.py'
 path3 = 'src/scraper/buscalibre.py'
+path4 = 'src/scraper/contrapunto.py'
 
 def __main__():
     subprocess.run(['python', path1])
     subprocess.run(['python', path2])
     subprocess.run(['python', path3])
+    subprocess.run(['python', path4])
 
 __main__()
 
